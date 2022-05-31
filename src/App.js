b30c619f4ecf3088755cb64795894ec7e131535c
ARoble <abdulladif0042@gmail.com>
2022-05-31 20:19:26 +0300

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Components/Header";
+import Search from "./Components/Search";
+import MovieCard from "./Components/MovieCard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classNameName="App">
+      <Header />
+      <div className="main">
+        <Search />
+        <div className="movies-section">
+          {/* <!-- one card --> */}
+          <MovieCard />
+          {/* <!-- one card --> */}
+        </div>
+      </div>
     </div>
   );
 }
